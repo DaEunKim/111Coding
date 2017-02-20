e44e19b6605ccaf96d0589fc18c8bb434de382d4
DaEun Kim <ekdms717@kookmin.ac.kr>
2017-02-20 18:41:10 +0900

    예제

---
+++ hanulsampleAPP/hanulsampleAPP/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  hanulsampleAPP
+//
+//  Created by 김다은 on 2017. 2. 18..
+//  Copyright © 2017년 김다은. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBOutlet weak var dani: UILabel!
+}
+
