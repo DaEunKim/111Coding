8c97593b5377d93f2f395b9a0af8d1f525e02532
DaEun Kim <ekdms717@kookmin.ac.kr>
2017-04-02 00:36:05 +0900

    adding the rest

---
+++ Eratostehnes/Eratostehnes/main.cpp
@@ -0,0 +1,27 @@
+//
+//  main.cpp
+//  Eratostehnes
+//
+//  Created by 김다은 on 2017. 2. 4..
+//  Copyright © 2017년 김다은. All rights reserved.
+//
+
+#include <iostream>
+#include <cstdio>
+#include <stack>
+using namespace std;
+
+int main(void){
+    int N, K;
+    scanf("%d %d", &N, &K);
+    int arr[1000]={0,};
+    stack<int> s;
+    
+    for(int i= 0;i<N-1;i++){
+        arr[i] = i+2;
+        s.push(i+2);
+        
+    }
+    for(int)
+    
+}
---
+++ 수학숙제/수학숙제/main.cpp
@@ -0,0 +1,28 @@
+//
+//  main.cpp
+//  수학숙제
+//
+//  Created by 김다은 on 2017. 2. 20..
+//  Copyright © 2017년 김다은. All rights reserved.
+//
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main(void){
+    int N;
+    scanf("%d", &N);
+    
+    while (N--) {
+        int number;
+        char ch[101];
+        
+        scanf("%s", ch);
+        for(int i = 0;i<101;i++){
+            
+        }
+    }
+    
+    
+}
