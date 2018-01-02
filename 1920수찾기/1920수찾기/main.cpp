816ee567911e3419b80ab4adf92b40c276141dbc
DaEunKim <ekdms717@naver.com>
2018-01-02 17:51:58 +0900

    1920수찾기

--- 1920수찾기/1920수찾기/main.cpp
+++ 1920수찾기/1920수찾기/main.cpp
@@ -7,9 +7,57 @@
 //
 
 #include <iostream>
+#include <algorithm>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int binarySearch(int N, int M, int *find_arr, int comp){
+    int left = 0 ;
+    int right = M;
+    int result=0;
+    while (left <= right){
+        int mid = (left + right) / 2;
+        if (find_arr[mid] > comp)
+            right = mid - 1;
+        else if (find_arr[mid] < comp)
+            left = mid + 1;
+        else{
+            result = mid;
+            break;
+        }
+    }
+    return result;
+
+}
+
+int main(void){
+    int N;
+    cin >> N;
+    int arr[100001] = {0,};
+    for(int i = 0;i<N;i++){
+        cin >> arr[i];
+    }
+    int M;
+    cin >> M;
+    int find_arr[100001] = {0,};
+    for(int i = 0;i<M;i++){
+        cin >> find_arr[i];
+    }
+    int tmp[100001] = {0,};
+    for(int i = 0;i<M;i++){
+        tmp[i] = find_arr[i];
+    }
+
+    sort(find_arr, find_arr+M);
+    
+    for(int i = 0;i<M;i++){
+        cout<< find_arr[i] <<" ";
+    }
+    cout<<endl;
+    int result = 0;
+    for(int i = 0;i<N;i++){
+        result = binarySearch(N, M, find_arr, arr[i]);
+        cout<< result<<endl;
+    }
+    
+    
 }
