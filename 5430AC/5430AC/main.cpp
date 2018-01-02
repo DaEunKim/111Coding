657af5d74a9dcf6faa35ab0768b4f8c1b3b6deaa
DaEunKim <ekdms717@naver.com>
2018-01-02 13:47:55 +0900

    5430AC

---
+++ 5430AC/5430AC/main.cpp
@@ -0,0 +1,86 @@
+//
+//  main.cpp
+//  5430AC
+//
+//  Created by 김다은 on 2017. 12. 28..
+//  Copyright © 2017년 김다은. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include <deque>
+#include <queue>
+using namespace std;
+
+deque<char> swap(deque<char> q){
+    deque<char> tmp_q;
+    while (!q.empty()) {
+        tmp_q.push_back(q.back());
+        q.pop_back();
+    }
+    return tmp_q;
+}
+
+void print(deque<char> q)
+{
+    cout<< "[";
+
+    unsigned long long q_size = q.size();
+    for(int i = 0;i<q_size;i++){
+        if(q.front()!=' '){
+            cout<< q.front();
+            q.pop_front();
+            if(q.empty())
+                cout<< "]"<<endl;
+            else{
+                cout<< ",";
+            }
+        }
+        if(q.empty()){
+            cout<< "error"<<endl;
+            break;
+        }
+    }
+
+}
+
+
+int main(void){
+    int T; cin >> T;
+    for(int z = 0;z<T;z++){
+        string p; cin >> p;
+        int n; cin >> n;
+        string x; cin >> x;
+        
+        char arr[100001];
+        char arr_x[100001];
+        
+        deque<char> q;
+
+        unsigned long long size = p.size();
+        for(int i = 0;i<size;i++){
+            arr[i] = p.at(i);
+        }
+        unsigned long long size_x = x.size();
+        for(int i = 0;i<size_x;i++){
+            arr_x[i] = x.at(i);
+        }
+
+        for(int i = 0;i<size_x;i++){
+            if(arr_x[i] != '[' && arr_x[i] != ']' && arr_x[i] != ','){
+                q.push_back(arr_x[i]);
+            }
+        }
+
+        for(int i = 0;i<size;i++){
+            if(arr[i]=='R'){
+                swap(q);
+            }
+            else if(arr[i]=='D'){
+                q.pop_front();
+            }
+        }
+        print(q);
+    }
+    
+}
