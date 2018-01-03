fc50448e4ffa1b4053ef9e2638afd270cffe9862
DaEunKim <ekdms717@naver.com>
2018-01-03 17:19:31 +0900

    1920수찾기

--- 1920수찾기/1920수찾기/main.cpp
+++ 1920수찾기/1920수찾기/main.cpp
@@ -12,82 +12,82 @@
 #include <vector>
 using namespace std;
 
-
-vector<pair<int, int>> binarySearch(vector<pair<int, int>> target, vector<pair<int, int>> comparison){
-    unsigned long comp_size = comparison.size();
-    unsigned long target_size = target.size();
-
-    unsigned long left = 0 ;
-    unsigned long right = target_size;
-    unsigned long result=0;
-    vector<pair<int, int>> tmp;
-    tmp = target;
-    for(int i = 0 ;i<comp_size;i++){
-
-        while (left <= right){
-            unsigned long mid = (left + right) / 2;
-            if (target[mid].first < comparison[i].first){
-                right = mid - 1;
-//                tmp[right].first = 0;
-            }
-            else if (target[mid].first > comparison[i].first){
-                left = mid + 1;
-//                tmp[left].first = 0;
-            }
-
-            else {
-                result = mid;
-                tmp[result].first = 1;
-            }
-        }
-        if(left>right)
-            tmp[left].first = 0;
-
-
-    }
-
-
-    return tmp;
-}
+//
+//vector<pair<int, int>> binarySearch(vector<pair<int, int>> target, vector<pair<int, int>> comparison){
+//    unsigned long comp_size = comparison.size();
+//    unsigned long target_size = target.size();
+//
+//    unsigned long left = 0 ;
+//    unsigned long right = target_size;
+//    unsigned long result=0;
+//    vector<pair<int, int>> tmp;
+//    tmp = target;
+//    for(int i = 0 ;i<comp_size;i++){
+//
+//        while (left <= right){
+//            unsigned long mid = (left + right) / 2;
+//            if (target[mid].first < comparison[i].first){
+//                right = mid - 1;
+////                tmp[right].first = 0;
+//            }
+//            else if (target[mid].first > comparison[i].first){
+//                left = mid + 1;
+////                tmp[left].first = 0;
+//            }
+//
+//            else {
+//                result = mid;
+//                tmp[result].first = 1;
+//            }
+//        }
+//        if(left>right)
+//            tmp[left].first = 0;
+//
+//
+//    }
+//
+//
+//    return tmp;
+//}
 
 int main(void){
-    vector<pair<int, int>> comparison;
+    vector<long long> comparison;
     
     int N;
     cin >> N;
     
-    int arr[100001] = {0,};
+    long long arr[100001] = {0,};
     for(int i = 0;i<N;i++){
         cin >> arr[i];
-        comparison.push_back(make_pair(arr[i], i));
+        comparison.push_back(arr[i]);
     }
     
-    vector<pair<int, int>> target;
+    vector<long long> target;
 
     int M;
     cin >> M;
-    int find_arr[100001] = {0,};
+    long long find_arr[100001] = {0,};
     for(int i = 0;i<M;i++){
         cin >> find_arr[i];
-        target.push_back(make_pair(find_arr[i], i));
+        target.push_back(find_arr[i]);
     }
-    sort(target.begin(), target.end());
+//    sort(target.begin(), target.end());
     
     //
-//    for(vector<int>::iterator i = target.begin();i!= target.end();i++){
-//        if(binary_search(comparison.begin(), comparison.end(), *i)){
-//            cout<< 1 <<endl;
-//        }
-//        else{
-//            cout<< 0 <<endl;
-//        }
-//    }
-//
-    for (auto i = 0; i < N; i++){
-        cout<< binarySearch(target, comparison)[i].first <<endl;
-
-//        cout << comparison[i].first  << ", " <<  comparison[i].second << endl;
+    for(vector<long long>::iterator i = target.begin(); i!= target.end();i++){
+        if(binary_search(comparison.begin(), comparison.end(), *i)){
+            cout<< 1 <<endl;
+        }
+        else{
+            cout<< 0 <<endl;
+        }
     }
+//
+//    for (auto i = 0; i < N; i++){
+//        cout<< binarySearch(target, comparison)[i].first <<endl;
+//
+////        cout << comparison[i].first  << ", " <<  comparison[i].second << endl;
+//    }
 //
 //    sort(target[0].second, target[M].second);
 //
