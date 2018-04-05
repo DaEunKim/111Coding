814ae4dfbb9cb1099ed88d6bc73c5f4669dbc526
DaEunKim <ekdms717@naver.com>
2018-04-05 17:12:36 +0900

    1260DFS와BFS/

--- k1/k1/main.cpp
+++ k1/k1/main.cpp
@@ -1,15 +1,35 @@
-//
-//  main.cpp
-//  k1
-//
-//  Created by 김다은 on 2018. 3. 31..
-//  Copyright © 2018년 김다은. All rights reserved.
-//
+#include <cstdio>
+using namespace std;
+/*
+ * Time complexity: O(n^2)
+ * Space complexity: O(n^2)
+ */
 
-#include <iostream>
+int N;
+int x,y;
+int adj[100001][100001];
+int vis[100001];
+int cnt =0;
+
+void dfs(int cur);
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    scanf("%d\n", &N);
+    for(int i = 0;i<N;i++){
+        scanf("%d %d", &x, &y);
+        adj[x][y] = adj[y][x] = 1;
+    }
+    dfs(adj[x][y]);
+    printf("%d\n", cnt);
+    
     return 0;
+}
+void dfs(int cur){
+    cnt++;
+    vis[cur] = 1;
+    for(int i = 1;i<N;i++){
+        if(adj[cur][i] && !vis[i]){
+            dfs(i);
+        }
+    }
 }
