b3c606e3d355e12a55cbcf652f7fc28005c3d3ca
DaEunKim <ekdms717@naver.com>
2018-04-05 17:11:55 +0900

    14888연산자끼워넣기/

---
+++ k1/k1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  k1
+//
+//  Created by 김다은 on 2018. 3. 31..
+//  Copyright © 2018년 김다은. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
