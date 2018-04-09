a5ea53e72bebdc8c36396654d852463202201c47
DaEunKim <ekdms717@naver.com>
2018-04-09 15:23:07 +0900

    S_숫자만들기

--- S_숫자만들기/S_숫자만들기/main.cpp
+++ S_숫자만들기/S_숫자만들기/main.cpp
@@ -5,20 +5,72 @@
 //  Created by 김다은 on 2018. 3. 27..
 //  Copyright © 2018년 김다은. All rights reserved.
 //
-
 #include <iostream>
+#include <queue>
+#include <algorithm>
 using namespace std;
 
-int main(void){
-    int T;
-    cin >> T;
-    int num;
-    cin >> num;
-    int plus_num, minus_num, mul_num, div_num;
-    cin >> plus_num >> minus_num >> mul_num >> div_num;
-    
-    
-    for(int i = 0;i<num;i++){
-        
+int n;
+int vis[101];
+int opt[101];
+int numbers[101]= {0, };
+int max_value = -1000000000;
+int min_value = 1000000000;
+
+void dfs(int cur, int idx, int num, int size);
+
+int main(int argc, const char * argv[]) {
+    cin >> n;
+    for(int i = 0;i<n;i++){
+        cin >> numbers[i];
     }
+    int idx = 0;
+    for(int i = 0;i<4;i++){
+        int cnt;
+        cin >> cnt;
+        for(int j = 0;j<cnt;j++){
+            opt[idx++] = i+1;
+        }
+    }
+    
+    dfs(0, 1, numbers[0], 0);
+    cout<< max_value <<endl;
+    cout<< min_value <<endl;
 }
+
+void dfs(int cur, int idx, int num, int size){
+    int result = 0;
+    
+    if(size == n-1){
+        max_value = max(num, max_value);
+        min_value = min(num, min_value);
+    }
+    else{
+        for(int i = 0; i < n-1; i++){
+            if(!vis[i]){
+                switch (opt[i]) {
+                    case 1:
+                        result = num + numbers[idx];
+                        break;
+                    case 2:
+                        result = num - numbers[idx];
+                        break;
+                    case 3:
+                        result = num * numbers[idx];
+                        break;
+                    case 4:
+                        result = num / numbers[idx];
+                        break;
+                    default:
+                        break;
+                }
+                vis[i] = true;
+                dfs(i, idx+1, result, size+1);
+            }
+        }
+    }
+    
+    //backtracking
+    vis[cur] = false;
+}
+
